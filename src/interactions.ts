d456a291f6c8f7c54b4562d2c4427552586cd2d9
DAllen <delank@protonmail.com>
2021-08-19 00:02:42 -0600

    animal pictures

--- src/interactions.ts
+++ src/interactions.ts
@@ -23,10 +23,16 @@ const helloCommand = (value: ApplicationCommandOptionValue) => {
 };
 
 const animalCommand = (value: ApplicationCommandOptionValue) => {
+  if (value.toString().includes("penguin"))
+    value = "http://nfs.stvfiles.com/imagebase/196/master/196570-puffling-young-puffin-st-kilda.jpg"
+  else if (value.toString().includes("dog"))
+    value = "https://www.pets4homes.co.uk/images/breeds/374/large/40da9a5b9c84a7c36b2169fd870fc1f7.jpg"
+  else if (value.toString().includes("cat"))
+    value = "https://www.pressandjournal.co.uk/wp-content/uploads/sites/2/2014/11/Harry-the-cat-2.jpg"
   const intResponse: InteractionResponse = {
     type: 4,
     data: {
-      content: `I am a ${value}`
+      content: `${value}`
     }
   }
   return json(intResponse);
