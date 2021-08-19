ce57eab884f1f5a810bc560a55a3f8e1403678d9
DAllen <delank@protonmail.com>
2021-08-19 12:45:05 -0600

    testing embeds

--- src/interactions.ts
+++ src/interactions.ts
@@ -11,26 +11,67 @@ import {
   ValueData,
 } from "./structures/index.ts";
 
+const memeEmbed = {
+	"content": "FortheMemelord",
+	"tts": false,
+	"components": [
+		{
+			"type": 1,
+			"components": [
+				{
+					"custom_id": "row_0_select_0",
+					"placeholder": "Selectameme",
+					"options": [
+						{
+							"label": "meme1",
+							"value": "https: //i.imgflip.com/5k4wae.jpg",
+							"default": false
+						},
+						{
+							"label": "meme2",
+							"value": "https: //i.imgflip.com/5k4x2t.jpg",
+							"default": false
+						}
+					],
+					"min_values": 1,
+					"max_values": 1,
+					"type": 3
+				}
+			]
+		}
+	],
+	"embeds": [
+		{
+			"type": "rich",
+			"title": "Selectyourmeme",
+			"description": "Memesreturnedfromsearch",
+			"color": "0xdd00ff",
+			"fields": [
+				{
+					"name": "meme1",
+					"value": "https: //i.imgflip.com/5k4wae.jpg"
+				},
+				{
+					"name": "meme2",
+					"value": "https: //i.imgflip.com/5k4x2t.jpg"
+				}
+			],
+			"image": {
+				"url": "https: //i.imgflip.com/5k4x2t.jpg",
+				"height": 0,
+				"width": 0
+			}
+		}
+	]
+}
+
 const helloCommand = (value = {}) => {
   return json({
     // Type 4 responds with the below message retaining the user's
     // input at the top.
     type: 4,
     data: {
-      content: `Hello, ${value}, this is the new slash command!`,
-      components: [
-        {
-          type: 1,
-          components: [
-            {
-              type: 2,
-              label: "Click me!",
-              style: 1,
-              custom_id: "click_one",
-            },
-          ],
-        },
-      ],
+      memeEmbed
     },
   });
 };
