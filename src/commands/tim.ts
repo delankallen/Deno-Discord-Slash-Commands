162dc0557f20ed1c525ddb2f956a49cd85774ed0
delankallen <delank@protonmail.com>
2022-02-15 13:08:25 -0700

    better fix

--- src/commands/tim.ts
+++ src/commands/tim.ts
@@ -2,11 +2,11 @@ import { json } from "https://deno.land/x/sift@0.3.5/mod.ts";
 import { InteractionResponse, ValueData } from "../structures/index.ts";
 
 const timCommand = async (options: ValueData[]) => {
-  const { value } = options[0];
+  const { value } = options[0] ?? "The world";
   const interactionResp: InteractionResponse = {
     type: 4,
     data: {
-      content: `${value ?? "The world"} has disappointed Tim. https://i.imgflip.com/5lo3yw.jpg`,
+      content: `${value} has disappointed Tim. https://i.imgflip.com/5lo3yw.jpg`,
     },
   };
   return await json(interactionResp);
